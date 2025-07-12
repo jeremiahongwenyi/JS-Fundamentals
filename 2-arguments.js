e0337b84afce8236d21a96e49568ba2582dcc084
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-07-13 01:05:34 +0300

    splve using if else statements

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,13 +1,22 @@
 const argumentsPassed = ['a', 'b', 8];
 const argumentLength = argumentsPassed.length
 
-switch(argumentLength){
-    case 0:
-        console.log("No argument");
-        break;
-        case 1:
-            console.log("Argument found");
-            break;
-            default :
-            console.log("Arguments found");
-        }
+if(argumentLength === 0){
+     console.log("No argument");
+} else if (argumentLength === 1){
+    console.log("Argument found")
+
+} else {
+    console.log("Arguments found")
+}
+
+// switch(argumentLength){
+//     case 0:
+//         console.log("No argument");
+//         break;
+//         case 1:
+//             console.log("Argument found");
+//             break;
+//             default :
+//             console.log("Arguments found");
+//         }
