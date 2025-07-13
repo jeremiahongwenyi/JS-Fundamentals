a29dff310b9f58f5ffff6021fc6036b115292777
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-07-13 10:28:14 +0300

    question 7

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2)
+const toNumber = parseInt(args[0])
+let i = 0
+
+if(!toNumber){
+    console.log("Missing number of occurrences");   
+} else {
+    while(i<toNumber){
+        console.log("C is fun");
+        i++;
+    }
+}
