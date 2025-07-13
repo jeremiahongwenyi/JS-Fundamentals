fe075c8cb1930ed76ac41b301351eb9a42cb71fd
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-07-13 10:11:59 +0300

    add question 6

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+// myArray.forEach( (element)=>{
+//     console.log(element)
+// })
+
+for(let i = 0; i<myArray.length; i++){
+    console.log(myArray[i]);
+    
+}
