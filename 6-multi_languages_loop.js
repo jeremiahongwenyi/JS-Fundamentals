9f2b3e6a27822ba41f6a5621b800c3cb92845e6a
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-07-13 10:17:52 +0300

    add question 6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-
-
-for(let i = 0; i<myArray.length; i++){
+for (let i = 0; i<myArray.length; i++){
     console.log(myArray[i]);
     
 }
