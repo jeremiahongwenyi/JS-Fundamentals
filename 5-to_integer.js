6d8514ec193f744448e7d0bfe41472ed4a59ca15
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-07-13 09:53:10 +0300

    fixed bug in question 5

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 const toNumber = parseInt(args[0])
 
 if(toNumber){
-    console.log(` My number: ${toNumber}`);   
+    console.log(`My number: ${toNumber}`);   
 } else {
     console.log("Not a number");
     
