f115e3ee7890b047ca4f3f72f3c7542bd178a64f
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-07-13 09:58:47 +0300

    fixed bug in question 5

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,11 +1,9 @@
 const passedArguments  = process.argv.slice(2)
 console.log(passedArguments);
 
-if(passedArguments===0){
+if(!passedArguments[0]){
     console.log("No argument");    
-} else if (passedArguments.length === 1){
-    console.log("Argument found");    
-} else {
-    console.log("Arguments found");
+}  else {
+    console.log(`${passedArguments[0]}`);
     
 }
