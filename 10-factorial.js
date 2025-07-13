fee23b91046b7cf0eea04c675679f20f0a1bdf8c
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-07-13 21:09:17 +0300

    question 10 correction to use recursion

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,21 +1,15 @@
 const args = process.argv.slice(2)
-const startingNumber = args[0];
-let factorialValue = 1;
-factorial(startingNumber);
+const startingNumber = parseInt(args[0]);
+
+let factorialValue  = factorial(startingNumber);
 
 
 function factorial (fact){
-
-    if(fact === NaN){
-        factorialValue = 1
-        console.log(factorialValue);       
-    } else {
-        for (let i = startingNumber; i > 0; i--){
-            factorialValue = factorialValue * i
-        }
-
-        console.log(factorialValue);
-        
-    }
-    
+    if(isNaN(fact) || fact === 0){
+            return 1
+    }  else {
+        return fact * factorial(fact-1)   
+    }    
 }
+
+console.log(factorialValue);
